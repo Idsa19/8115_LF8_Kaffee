e576b4f8cbe923af9172a06841d7d5533bf72eb1
Idsa19 <90633999+Idsa19@users.noreply.github.com>
2021-12-07 19:30:10 +0100

    Fertig!

--- Zuweisungs Operatoren.py
+++ Zuweisungs Operatoren.py
@@ -3,19 +3,26 @@ y = int(input("Geben Sie die 2. Zahl ein: "))
 
 z = x
 z += y
-print("Addition: {0}+={1} =>{2}".format(x, y, z))
+print("\nAddition:                        {0}+={1} =>{2}".format("{0:08b}".format(x), "{0:08b}".format(y), "{0:08b}".format(z)))
+print("Addition:                        {0}+={1} =>{2}\n".format(x, y, z))
 z = x
 z &= y
-print("Bitwise AND on operands:         {0}&={1} =>{2}".format(x, y, z))
+print("Bitwise AND on operands:         {0}&={1} =>{2}".format("{0:08b}".format(x), "{0:08b}".format(y), "{0:08b}".format(z)))
+print("Bitwise AND on operands:         {0}&={1} =>{2}\n".format(x, y, z))
 z = x
 z |= y
-print("Bitwise OR on operands:          {0}|={1} =>{2}".format(x, y, z))
+print("Bitwise OR on operands:          {0}|={1} =>{2}".format("{0:08b}".format(x), "{0:08b}".format(y), "{0:08b}".format(z)))
+print("Bitwise OR on operands:          {0}|={1} =>{2}\n".format(x, y, z))
 z = x
 z ^= y
-print("Bitwise xOR on operands:         {0}^={1} =>{2}".format(x, y, z))
+hi = z
+print("Bitwise xOR on operands:         {0}^={1} =>{2}".format("{0:08b}".format(x), "{0:08b}".format(y), "{0:08b}".format(z)))
+print("Bitwise xOR on operands:         {0}^={1} =>{2}\n".format(x, y, z))
 z = x
 z >>= y
-print("Bitwise right shift on operands: {0}>>={1} =>{2}".format(x, y, z))
+print("Bitwise right shift on operands: {0}>>={1} =>{2}".format("{0:08b}".format(x), "{0:08b}".format(y), "{0:08b}".format(z)))
+print("Bitwise right shift on operands: {0}>>={1} =>{2}\n".format(x, y, z))
 z = x
 z <<= y
-print("Bitwise left shift on operands:  {0}<<={1} =>{2}".format(x, y, z))
+print("Bitwise left shift on operands:  {0}<<={1} =>{2}".format("{0:08b}".format(x), "{0:08b}".format(y), "{0:08b}".format(z)))
+print("Bitwise left shift on operands:  {0}<<={1} =>{2}\n".format(x, y, z))
