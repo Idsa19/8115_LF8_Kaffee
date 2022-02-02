b30ed2ce30f73601472454478a8b9b1bfad7564a
Idsa19 <90633999+Idsa19@users.noreply.github.com>
2022-02-02 16:32:34 +0100

    Debugger

--- prim2.py
+++ prim2.py
@@ -3,13 +3,14 @@ eingabe = int(input("Bis zu welchem Wert sollen Primzahlen ausgebenen werden? ")
 
 while num < eingabe:
     primtest = True
-    if num != 1 & num != 2 & num != 3:
+    if num == 1:
+        primtest = False
+    else:
         i = 2
-        while i < num:
+        while i <= num:
             if num % i == 0:
                 primtest = False
-                break
             i += 1
-    if primtest:
-        print(num)
+            if primtest == True:
+                print(num)
     num += 1
