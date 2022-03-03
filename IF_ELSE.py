509ab4bda00004e08f1b42b37071d87f2c9bc181
Idsa19 <90633999+Idsa19@users.noreply.github.com>
2022-03-03 19:11:13 +0100

    hi

--- IF_ELSE.py
+++ IF_ELSE.py
@@ -1,5 +1,11 @@
-wort="start"
-if wort=="start":
-   x="los"
-else:
-   x="halt"
+x = int(input("Zahl eingeben: "))
+if x < 100:
+    print("Die Zahl ist kleiner 100")
+elif x == 100:
+    print("Die Zahl ist 100")
+elif x > 100:
+    print("Die Zahl ist größer 100")
+if (x % 2) == 0:
+    print("Die Zahl ist gerade")
+elif (x % 2) != 0:
+    print("Die Zahl ist ungerade")
