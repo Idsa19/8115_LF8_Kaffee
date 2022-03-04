8458e2ce04209bd14809d9a112e704bd566cadb4
Idsa19 <90633999+Idsa19@users.noreply.github.com>
2022-03-04 15:58:13 +0100

    hi

---
+++ For-Loop_gerade_ungerade.py
@@ -0,0 +1,6 @@
+print("Gerade Zahlen: ", end='')
+for x in range(2, 101, 2):
+    print(str(x) + ", ", end='')
+print("\nUngerade Zahlen: ", end='')
+for x in range(1, 101, 2):
+    print(str(x) + ", ", end='')
