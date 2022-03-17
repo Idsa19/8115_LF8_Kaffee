3e2d3553cbf534b25dd4d5840cab690617be60a0
Idsa19 <90633999+Idsa19@users.noreply.github.com>
2022-03-17 17:49:33 +0100

    hi

--- Funktion_gerade_ungerade.py
+++ Funktion_gerade_ungerade.py
@@ -1,11 +1,9 @@
 def gerade (n):
-    if (n%2) == 1:
+    if (n%2) == 0:
         return True
     return False
 
-#    print("\nUngerade Zahlen: ", end='')
-#    for x in range(1, 101, 2):
-#        print(str(x) + ", ", end='')
 
-print("Gerade Zahlen: ", end='')
-print(", ", end='')
+print("Die Zahlen von 1 - 100: ")
+for x in range(1, 101, 1):
+    print(str(x), str(gerade(x)) + ", ")
